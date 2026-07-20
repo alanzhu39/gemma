0a7cb5011a9c1e84ce656b35110de572027ac95c
Alan Zhu <alan.zhu39@gmail.com>
2026-07-19 17:09:29 -0700

    updating benchmark

--- src/routes/performance/+page.svelte
+++ src/routes/performance/+page.svelte
@@ -57,7 +57,7 @@
 		const start = performance.now();
 
 		const context = "Here is a Python function to find the nth Fibonacci number:";
-		const steps = 10;
+		const steps = 20;
 		const output = runInference(model, tokenizer, context, steps);
 
 		const elapsed = performance.now() - start;
